40e8fd7f940b9f9fb0b8e4cc0066160b627d9f0e
razieh jafari <razie.jafari68@gmail.com>
2021-05-20 13:49:34 +0430

    add the files in the local repository

---
+++ vueProject/zayShop/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="nav-bar" >
+    <p class="bg-indigo-light">hello</p>
+   
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Navbar',
+  
+}
+</script>
+
+<style>
+#nav-bar{
+}
+</style>
---
+++ vueProject/zayShop/tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      indigo: {
+        light: '#b3bcf5',
+        DEFAULT: '#5c6ac4',
+        dark: '#202e78',
+      }
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+      
+    },
+  
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+}
