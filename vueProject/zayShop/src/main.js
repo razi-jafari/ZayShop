bc73bdc1c7d334a684edf31f1f4d1869761d0fc9
razieh jafari <razie.jafari68@gmail.com>
2021-06-04 21:04:11 +0430

    modified main.js

--- vueProject/zayShop/src/main.js
+++ vueProject/zayShop/src/main.js
@@ -4,16 +4,17 @@ import './index.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import Routes from './routers'
-
+import router from './routers'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
 library.add(fas)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-Vue.use(Routes)
+// Vue.use(Routes)
 // import { create } from 'core-js/fn/object'
 
 Vue.config.productionTip = false
 
 new Vue({
-  Routes,
+  router,
   render: h => h(App),
 }).$mount('#app')
